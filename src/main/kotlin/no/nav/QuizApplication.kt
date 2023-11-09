022bd927ca9f5f3d3055a1895aff4033f74c1bab
Jacob Sjødin <jacobsjodin@Jacobs-MacBook-Pro.local>
2023-11-09 14:09:32 +0100

    TEam navn

--- src/main/kotlin/no/nav/QuizApplication.kt
+++ src/main/kotlin/no/nav/QuizApplication.kt
@@ -34,7 +34,7 @@ class QuizApplication(private val teamName: String, database: Database? = null):
 
     private fun handleRegisterTeam(question: Question) {
         //TODO("Her må du skrive kode ;)")
-        //answer(question.category, questionId = question.id(), "DIN HEX")
+        //answer(question.category, questionId = question.id(), "FAFAFA")
     }
 
 }
